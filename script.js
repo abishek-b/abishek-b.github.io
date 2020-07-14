e87845e5c3e008da073ca1d60f5034cead648076
abisuman <abi.suman@gmail.com>
2020-07-14 21:26:30 +0530

    responsive css

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
-$( document ).on( 'scroll', function () {
+$(document).on('scroll', function () {
     $navbar = $(".navbar")
-    if ( $( document ).scrollTop() > 583 ) {
-        $navbar.addClass( 'navbar-bg-fixed' );
+    if ($(document).scrollTop() > 583) {
+        $navbar.addClass('navbar-bg-fixed');
     } else {
-        $navbar.removeClass( 'navbar-bg-fixed' );
+        $navbar.removeClass('navbar-bg-fixed');
     }
-} );
+});
+
+
+document.getElementById('btn_hire_me').addEventListener("click", function (e) {
+    // e.preventDefault()
+    location.href = "mailto:bjabishek30@gmail.com?subject=Inquiry - " + document.getElementById('form_name').value + "("+document.getElementById('form_email').value + ")&body=" + document.getElementById('form_message').value
+})
