18b7663b5cb0b1b4fead6999b18d15564f87af99
abisuman <abi.suman@gmail.com>
2020-07-14 22:47:13 +0530

    responsive css - part 3

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ document.getElementById('btn_hire_me').addEventListener("click", function (e) {
     // e.preventDefault()
     location.href = "mailto:bjabishek30@gmail.com?subject=Inquiry - " + document.getElementById('form_name').value + "("+document.getElementById('form_email').value + ")&body=" + document.getElementById('form_message').value
 })
+
+const hamburger = document.querySelector('.navbar .hamburger')
+const mobile_menu = document.querySelector('.navbar ul')
+
+
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active")
+    mobile_menu.classList.toggle("active")
+})
